c2662435a5bff97274d8d38c9978a2d10b8af2af
Jareth Gonzalez Prieto <jarethprieto7@gmail.com>
2026-06-12 10:31:35 -0600

    Fix import formatting in MainActivity.kt

--- first_flutter_project/android/app/src/main/kotlin/com/example/first_flutter_project/MainActivity.kt
+++ first_flutter_project/android/app/src/main/kotlin/com/example/first_flutter_project/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.first_flutter_project
 
-import io.flutter.embedding.android.FlutterActivity
+   import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
